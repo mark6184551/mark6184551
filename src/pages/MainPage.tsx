8576584f0154cbba5e681cb2c4f4c0c3c78b73d0
mark6184551 <32125966+mark6184551@users.noreply.github.com>
2023-07-23 22:55:41 +0800

    Add files via upload

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import Title from "../components/title"
+import Title2 from '../components/title2'
+
+
+export default function MainPage() {
+
+    return (
+        <>
+            <Title2 />
+        </>
+    )
+}
